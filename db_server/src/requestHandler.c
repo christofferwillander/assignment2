338f8bd8b99583fc6d4b5241f8db5b543f33f4ee
Christoffer <christoffer.willander@gmail.com>
2020-09-23 15:05:14 +0200

    Basic implementation of request handler.

---
+++ db_server/src/requestHandler.c
@@ -0,0 +1,32 @@
+#include "../include/request.h"
+#include "createTable.c"
+#include "insertToTable.c"
+#include "listTables.c"
+#include "selectTable.c"
+#include "showSchema.c"
+extern const char databasePath[];
+void handleRequest(request_t *request, int clientSocket) {
+    switch (request->request_type)
+        {
+        case RT_CREATE:
+            create(request);
+            break;
+        case RT_SCHEMA:
+            getSchema(request);
+            break;
+        case RT_INSERT:
+            insert(request);
+            break;
+        case RT_SELECT:
+            selectTable(request);
+            break;
+        case RT_TABLES:
+            listTables(databasePath, clientSocket);
+            break;
+        default:
+            printf("[-] Unknown request received\n");
+            break;
+        }
+    
+    destroy_request(request);
+}
