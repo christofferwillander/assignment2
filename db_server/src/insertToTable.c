0de9e0a7366ee8a646b03d965b01c2bfab4a95c3
Christoffer <christoffer.willander@gmail.com>
2020-10-09 23:17:46 +0200

    Fixed insertion to table. Clean-up.

--- db_server/src/insertToTable.c
+++ db_server/src/insertToTable.c
@@ -15,11 +15,12 @@ extern char databasePath[];
 
 void insertToFile(FILE *ptr, char *value, int check);
 int numberOfColumns(FILE *ptr);
-int checkBuffersize(request_t *req, FILE *ptr);
+int validateInput(request_t *reqest, FILE *ptr, int clientSocket);
 
 int numberOfColumns(FILE *ptr)
 {
     int count = 0;
+
     char lineTemp[200];
     char *curPos;
 
@@ -28,114 +29,120 @@ int numberOfColumns(FILE *ptr)
 
     curPos = lineTemp;
 
-    while(strstr(curPos, ":") != NULL) {
-        curPos = strstr(curPos, ":") + 1;
+    while(strstr(curPos, "INT") != NULL) {
+        curPos = strstr(curPos, "INT") + strlen("INT");
         count++;
     }
 
-    count = count / 2;
+    curPos = lineTemp;
+
+    while(strstr(curPos, "VARCHAR") != NULL) {
+        curPos = strstr(curPos, "VARCHAR") + strlen("VARCHAR");
+        count++;
+    }
+
     return count;
 }
 
-int checkBuffersize(request_t *req, FILE *ptr)
-{
-    column_t *end;
-    column_t *temp = req->columns;
-    char *temp1 = NULL, *temp2 = NULL, *temp3 = NULL;
-    int len = 0;
-    int dig = 0;
-    int columns = numberOfColumns(ptr);
-    int *sizeArr = malloc(columns);
-    int sizecounter = 0;
-    int *sizeArrFromFile = malloc(columns);
-    int sizeFromFileCounter = 0;
-    int retval = -1 ;
-    fseek(ptr, 0, SEEK_SET);
-    //find all the sizes in order in the request
-    while (temp != NULL)
-    {   
-        if(temp->data_type == DT_VARCHAR)
-        {
-            printf("VARCHAR (%d)\n", sizecounter);
-            sizeArr[sizecounter++] = strlen(temp->char_val);
-        }
-        else if (temp->data_type == DT_INT) {
-            printf("INT (%d)\n", sizecounter);
-            sizeArr[sizecounter++] = -1;
-        }
+int validateInput(request_t *request, FILE *ptr, int clientSocket) {
+    int nrOfCols = numberOfColumns(ptr);
+    int statusCode = 0;
+    int inputCounter = 0, tableCounter = 0; 
 
-        end=temp->next;
-        temp = end;    
+    column_t *curCol = request->columns;
+    int *inputTypes = NULL, *inputSizes = NULL;
+    int *tableTypes = NULL, *tableSizes = NULL;
+
+    /* Allocating memory */
+    if (nrOfCols > 0) {
+        inputTypes = malloc(sizeof(int) * nrOfCols);
+        inputSizes = malloc(sizeof(int) * nrOfCols);
+        tableTypes = malloc(sizeof(int) * nrOfCols);
+        tableSizes = malloc(sizeof(int) * nrOfCols);
     }
 
-    temp1 = malloc(100);
-    fgets(temp1, 100, ptr);
-    temp3 = strstr(temp1, "\n");
-    len = (int)(temp3 - temp1) + 1;
-    temp2 = malloc(len);
-    memcpy(temp2, temp1, len);
 
-    //Find all the max sizes from the table file
+    while (curCol != NULL) {
+        if (curCol->data_type == DT_INT) {
+            inputTypes[inputCounter] = DT_INT;
+            inputSizes[inputCounter++] = -1;
+        }
+        else if (curCol->data_type == DT_VARCHAR) {
+            inputTypes[inputCounter] = DT_VARCHAR;
+            inputSizes[inputCounter++] = strlen(curCol->char_val);
+        }
 
-    char *strPtr1 = temp2, *strPtr2 = NULL;
+        curCol = curCol->next;
+    }
 
-    int strLength = 0;
-    int ctr = 0;
-    int retreiveNum = 0;
-    char *tempStr;
+    fseek(ptr, 0, SEEK_SET);
+    char *tempLine = malloc(sizeof(char) * 100);
+    fgets(tempLine, 100, ptr);
+
+    char *strPtr1 = NULL, *strPtr2 = NULL, *tempStr = NULL;
+    int strLength = 0, retrieveSize = 0;
+
+    strPtr1 = tempLine;
 
     while (strstr(strPtr1, ":") != NULL) {
         strPtr2 = strPtr1;
         strPtr1 = strstr(strPtr1, ":");
         strLength = (strPtr1 - strPtr2);
-        if (strLength > 0 && strPtr1 != NULL) {
-            tempStr = malloc(strLength + 1);
+
+        if (strLength > 0) {
+            tempStr = malloc((sizeof(char) * strLength) + 1);
             memcpy(tempStr, strPtr2, strLength);
             tempStr[strLength] = '\0';
             strPtr1 += 1;
 
             if (strcmp("INT", tempStr) == 0) {
-                ctr++;
+                tableSizes[tableCounter] = -1;
+                tableTypes[tableCounter++] = DT_INT;
             }
-            else if (strcmp("VARCHAR", tempStr) == 0) {
-                ctr++;
-                retreiveNum = 1;
+            else if (strcmp("VARCHAR", tempStr) == 0)
+            {
+                tableTypes[tableCounter] = DT_VARCHAR;
+                retrieveSize = 1;    
             }
-            else if (retreiveNum == 1 && ctr > 0) {
-                sizeArrFromFile[(ctr - 1)] = atoi(tempStr);
-                printf("VARCHAR SIZE %d DETECTED (index %d) \n", sizeArrFromFile[ctr - 1], ctr-1);
-                retreiveNum = 0;
+            else if (retrieveSize == 1) {
+                tableSizes[tableCounter++] = atoi(tempStr);
+                retrieveSize = 0;
             }
 
             free(tempStr);
         }
     }
-    //compare the sizes found, if the incomming size exceeds the max size it will not add anything to the table
-    for(int i = 0; (i < sizecounter) && (retval != 1); i++)
-    {
-        //printf("cmd-> %d\t vs\t org -> %d\n", sizeArrFromFile[i], sizeArr[i]);
-        if((sizeArr[i] <= sizeArrFromFile[i]) && (sizeArr[i] != -1))
-        {
-            printf("Correct varchar size (index %d)\n", i);
-            retval = 0;
-        }
-        else if (sizeArr[i] != -1) {
-            retval = 1;
-            printf("fuck\n");
+
+    free(tempLine);
+
+    /* Ensuring amount of input columns correspond to the amount in the table */
+    if (tableCounter != inputCounter) {
+        statusCode = 1;
+        send(clientSocket, "ERROR: Incorrect amount of columns in provided INSERT statement\n", sizeof("ERROR: Incorrect amount of columns in provided INSERT statement\n"), 0);
+    }
+
+    /* Checking that all data types match */
+    for (int i = 0; (i < nrOfCols) && statusCode != 1; i++) {
+        if (inputTypes[i] != tableTypes[i]) {
+            statusCode = 1;
+            send(clientSocket, "ERROR: Incorrect data types in provided INSERT statement\n", sizeof("ERROR: Incorrect data types in provided INSERT statement\n"), 0);
         }
     }
 
-    free(sizeArr);
-    free(sizeArrFromFile);
-    free(temp1);
-    free(temp2);
-    return retval;
+    /* Checking that sizes are in correct range */
+    for (int i = 0; (i < nrOfCols) && statusCode != 1; i++) {
+        if ((inputSizes[i] > tableSizes[i])) {
+            statusCode = 1;
+            send(clientSocket, "ERROR: Incorrect data sizes in provided INSERT statement\n", sizeof("ERROR: Incorrect data sizes in provided INSERT statement\n"), 0);
+        }
+    }
 
+    return statusCode;
 }
 
-void insertToFile(FILE *ptr, char *value, int check)
+void insertToFile(FILE *ptr, char *value, int isEOL)
 {
-    if(check != 1)
+    if(isEOL != 1)
     {
         fprintf(ptr, "%s:", value);
     }
@@ -154,56 +161,60 @@ void insertToTable(request_t *req, int clientSocket)
     char *filePath = malloc(strlen(databasePath) + strlen(req->table_name) + 1);
     strcpy(filePath, databasePath);
     strcat(filePath,req->table_name);
-    FILE *file = fopen(filePath, "a+");
-    doLock(fileno(file), LOCK, WRITE);
+    FILE *file;
 
     if(access(filePath, F_OK) == -1)
     {
-        send(clientSocket, "ERROR: Table does not exist\n", strlen("ERROR: Table does not exist\n"), 0);
+        send(clientSocket, "ERROR: Table does not exist\n", sizeof("ERROR: Table does not exist\n"), 0);
     }
-    else if (checkBuffersize(req, file) == 1)
-    {
-       send(clientSocket, "VARCHAR exceeds size\n", sizeof("VARCHAR exceeds size\n"), 0);
-    }
-    else
-    {
-        int columnsNr= 0;
-        int check = 0;
-        columnCount = numberOfColumns(file);
-
-        column_t *end;
-        column_t *temp = req->columns;
-        fseek(file, 0, SEEK_END);
-        while(temp != NULL)
-        {
-            columnsNr++;
-            if(columnsNr == columnCount)
-            {
-                check = 1;
-            }
-            if(temp->data_type != 0)
-            {
-                //data type is varchar
-                charValue=temp->char_val;
-                insertToFile(file, charValue, check); 
-
-            }
-            else
-            {
-                //data typ is int
-                intValue = malloc(sizeof(temp->int_val));
-                sprintf(intValue, "%d", temp->int_val);
-                insertToFile(file, intValue, check);
-                free(intValue);
-
-            }
-            end=temp->next;
-            temp = end;   
+    else {
+        file = fopen(filePath, "a+");
+        doLock(fileno(file), LOCK, WRITE);
+        if (validateInput(req, file, clientSocket) == 1) {
+            /* No bueno */
         }
-        send(clientSocket, "Successfully added to table\n", sizeof("Successfully added to table\n"), 0);
-        
+        else
+        {
+            int columnsNr= 0;
+            int isEOL = 0;
+            columnCount = numberOfColumns(file);
+
+            column_t *end;
+            column_t *temp = req->columns;
+            fseek(file, 0, SEEK_END);
+            while(temp != NULL)
+            {
+                columnsNr++;
+                if(columnsNr == columnCount)
+                {
+                    isEOL = 1;
+                }
+
+                if(temp->data_type != 0)
+                {
+                    //data type is varchar
+                    charValue=temp->char_val;
+                    insertToFile(file, charValue, isEOL); 
+
+                }
+                else
+                {
+                    //data typ is int
+                    intValue = malloc(sizeof(temp->int_val));
+                    sprintf(intValue, "%d", temp->int_val);
+                    insertToFile(file, intValue, isEOL);
+                    free(intValue);
+
+                }
+                end=temp->next;
+                temp = end;   
+            }
+            send(clientSocket, "Successfully added to table\n", sizeof("Successfully added to table\n"), 0);
+            
+        }
+        doLock(fileno(file), UNLOCK, WRITE);
+        fclose(file);
     }
-    doLock(fileno(file), UNLOCK, WRITE);
-    fclose(file);
+
     free(filePath);
 }
